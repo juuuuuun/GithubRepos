36770b94a0026197d624f9ca26b1badb8bb919f8
Jun Oh <kyungjunjoh@gmail.com>
2019-01-24 13:14:46 -0800

    Copied basic code to get my github repo through github's api

--- GithubRepos/ViewController.m
+++ GithubRepos/ViewController.m
@@ -17,6 +17,20 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    NSURL *url = [NSURL URLWithString:@"https://api.github.com/users/juuuuuun/repos"]; // 1
+    NSURLRequest *urlRequest = [[NSURLRequest alloc] initWithURL:url]; // 2
+    
+    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration]; // 3
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:configuration]; // 4
+    
+    NSURLSessionDataTask *dataTask = [session dataTaskWithRequest:urlRequest completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        
+        
+        
+    }]; // 5
+    
+    [dataTask resume]; // 6
 }
 
 
